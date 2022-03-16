bd4eea4c3bcb9843aa36b4f143059e540ec8ccd8
SiriusLL <sirius.love.light@gmail.com>
2022-03-16 16:54:47 -0600

    add network check, add total minted count, add mining isLoading gif

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
+import mining from "./assets/mine.gif";
 import React from "react";
 import { useEffect, useState } from "react";
 import { ethers } from "ethers";
@@ -13,6 +14,8 @@ const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [tokensMinted, setTokensMinted] = useState("");
+  const [isMining, setIsMining] = useState(false);
 
   const isWalletConnected = async () => {
     const { ethereum } = window;
@@ -27,6 +30,14 @@ const App = () => {
     const accounts = await ethereum.request({ method: "eth_accounts" });
 
     if (accounts.length !== 0) {
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      //String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby TestNetwork");
+        return;
+      }
       const account = accounts[0];
       console.log("Found an authorized account: ", account);
       setCurrentAccount(account);
@@ -44,12 +55,37 @@ const App = () => {
         return;
       }
 
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      //String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby TestNetwork");
+        return;
+      }
+
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkChainId = async () => {
+    try {
+      const { ethereum } = window;
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      //String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby TestNetwork");
+        return;
+      }
     } catch (error) {
       console.log(error);
     }
@@ -69,8 +105,40 @@ const App = () => {
     isWalletConnected();
   }, []);
 
+  const getTokensMinted = async () => {
+    const CONTRACT_ADDRESS = "0x90BE7ABbe65A13624200EEcCE504BBB7947Be622";
+
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          epicNft.abi,
+          signer
+        );
+
+        console.log("Get em while there hot!!!");
+        let remainingTxn = await connectedContract.getTokensRemaining();
+
+        // console.log("Mining...please wait.");
+        // await remainingTxn.wait();
+
+        console.log(`Minted: ${remainingTxn[0]}/${remainingTxn[1]}`);
+
+        setTokensMinted(`Minted: ${remainingTxn[0]}/${remainingTxn[1]}`);
+      } else {
+        console.log("ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getTokensMinted();
   const mintNftFromContract = async () => {
-    const CONTRACT_ADDRESS = "0x97f8d4183B9b6fe0DB59a645af057555ab4A070f";
+    const CONTRACT_ADDRESS = "0x90BE7ABbe65A13624200EEcCE504BBB7947Be622";
 
     try {
       const { ethereum } = window;
@@ -85,11 +153,12 @@ const App = () => {
         );
 
         console.log("Get ready to break your bank paying gas fees");
+        setIsMining(true);
         let nftTxn = await connectedContract.mintEpicNFT();
 
         console.log("Mining...please wait.");
         await nftTxn.wait();
-
+        setIsMining(false);
         console.log(
           `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
         );
@@ -111,6 +180,14 @@ const App = () => {
           </p>
           {!currentAccount ? (
             renderNotConnectedContainer()
+          ) : isMining ? (
+            <img
+              alt="Mining"
+              className="mine"
+              src={mining}
+              width="500"
+              height="auto"
+            />
           ) : (
             <button
               onClick={mintNftFromContract}
@@ -119,6 +196,7 @@ const App = () => {
               Mint NFT
             </button>
           )}
+          <div className="mint-count">{tokensMinted}</div>
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
