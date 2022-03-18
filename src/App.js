79e7e20aeaaddc42f43ad02d35195ca475996ac0
SiriusLL <sirius.love.light@gmail.com>
2022-03-18 15:54:47 -0600

    small final adjustments

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
+const CONTRACT_ADDRESS = "0x17a7Ed37F303B2d0b3389616F27b175e076Ec145";
+
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
   const [tokensMinted, setTokensMinted] = useState({});
@@ -41,6 +43,8 @@ const App = () => {
       const account = accounts[0];
       console.log("Found an authorized account: ", account);
       setCurrentAccount(account);
+
+      eventListener();
     } else {
       console.log("No authorized account found");
     }
@@ -70,6 +74,37 @@ const App = () => {
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
+
+      eventListener();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const eventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          epicNft.abi,
+          signer
+        );
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber());
+          alert(
+            `Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`
+          );
+        });
+
+        console.log("Setup event listener!");
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error);
     }
@@ -107,8 +142,6 @@ const App = () => {
   }, []);
 
   const getTokensMinted = async () => {
-    const CONTRACT_ADDRESS = "0x8F9c193aB2f8E775DbC0892AC23369133DBA22bd";
-
     try {
       const { ethereum } = window;
 
@@ -144,10 +177,17 @@ const App = () => {
   };
 
   const mintNftFromContract = async () => {
-    const CONTRACT_ADDRESS = "0x8F9c193aB2f8E775DbC0892AC23369133DBA22bd";
-
     try {
       const { ethereum } = window;
+
+      let chainId = await ethereum.request({ method: "eth_chainId" });
+
+      //String, hex code of the chainId of the Rinkebey test network
+      const rinkebyChainId = "0x4";
+      if (chainId !== rinkebyChainId) {
+        alert("You are not connected to the Rinkeby TestNetwork");
+        return;
+      }
 
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
@@ -170,6 +210,13 @@ const App = () => {
         console.log(
           `Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`
         );
+
+        // connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+        //   console.log(from, tokenId.toNumber());
+        //   alert(
+        //     `Hey there! We've minted your NFT. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: <https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}>`
+        //   );
+        // });
       } else {
         console.log("ethereum object doesn't exist!");
       }
@@ -204,11 +251,7 @@ const App = () => {
               height="auto"
             />
           ) : (
-            <button
-              onClick={mintNftFromContract}
-              className={soldout()}
-              disabled
-            >
+            <button onClick={mintNftFromContract} className={soldout()}>
               Mint NFT
             </button>
           )}
@@ -217,13 +260,26 @@ const App = () => {
           </div>
         </div>
         <div className="footer-container">
+          <div>
+          <button
+            className="cta-button opensea-button"
+            onClick={() =>
+              window.open(
+                "https://testnets.opensea.io/collection/starnft-oiiksmtuel",
+                "_blank"
+              )
+            }
+          >
+            See the Collection
+          </button></div>
+          <div className='tweet'>
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built on @${TWITTER_HANDLE}`}</a></div>
         </div>
       </div>
     </div>
